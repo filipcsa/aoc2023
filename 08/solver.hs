35bf6e87ef1e4dfdcbb2492ef5961d6f17f8dea1
Csaba Filip <filip.csaba@email.cz>
2024-12-10 22:04:25 +0100

    08a rework

--- 08/solver.hs
+++ 08/solver.hs
@@ -1,45 +1,25 @@
-import Prelude hiding (lookup)
-import Data.Map ( Map, insert, empty, lookup )
-import Data.Maybe (fromJust)
+import Text.Regex.TDFA ( (=~), AllMatches(getAllMatches), AllTextMatches (getAllTextMatches) )
+import qualified Data.Map as M
+import Data.List.Split (splitOn)
 
-type LRSucc = (String, String)
-type SuccMap = Map String LRSucc
-type Move = Char
-
-parseInput :: [String] -> ([Move], SuccMap)
-parseInput lines = (moves, succMap) where
-  moves = head lines
-  succMap = foldl parseSuccMap empty (drop 2 lines)
-
-parseSuccMap :: SuccMap -> String -> SuccMap
-parseSuccMap succMap str = newSuccMap where
-  key = take 3 str
-  left = take 3 $ drop 7 str
-  right = take 3 $ drop 12 str
-  newSuccMap = insert key (left, right) succMap
-
-countSteps :: [Move] -> SuccMap -> Int
-countSteps moves succMap = numMoves where
-  infMoves = cycle moves
-  numMoves = performMove infMoves "AAA" succMap
-
-performMove :: [Move] -> String -> SuccMap -> Int 
-performMove _ "ZZZ" _ = 0
-performMove (move:moves) curr succMap = 1 + performMove moves next succMap where 
-  next = getMoveSucc curr move succMap
-
-getMoveSucc :: String -> Move -> SuccMap -> String 
-getMoveSucc key move succMap = succ where 
-  lrSuccs = fromJust $ lookup key succMap
-  succ = case move of 
-    'L' -> fst lrSuccs
-    'R' -> snd lrSuccs
+type Trans = M.Map String (String, String)
 
 main :: IO ()
 main = do
-  contents <- getContents
-  let inputLines = lines contents
-  let (moves, succMap) = parseInput inputLines
-  print moves
-  print succMap
-  print $ countSteps moves succMap
+  content <- getContents
+  let inputLines = lines content
+  let [movesStrs, transStrs] = splitOn [""] inputLines
+  let (moves, trans) = (head movesStrs, foldl parseTrans M.empty transStrs)
+  print $ countMovesToZ moves trans "AAA" 0
+
+countMovesToZ :: String -> Trans -> String -> Int -> Int
+countMovesToZ _ _ "ZZZ" n = n
+countMovesToZ ms trans node n = countMovesToZ ms trans node' (n+1) where
+  mv = ms !! (n `mod` length ms)
+  (l, r) = M.findWithDefault (node, node) node trans
+  node' = if mv == 'L' then l else r
+
+parseTrans :: Trans -> String -> Trans
+parseTrans acc str = M.insert from (l, r) acc where
+  regex = "([A-Z]+) = \\(([A-Z]+), ([A-Z]+)\\)"
+  (_, _, _, [from, l, r]) = str =~ regex :: (String, String, String, [String])
