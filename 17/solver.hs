3efcbf047d12539b1e7e38ea581917eff00a5770
Csaba Filip <filip.csaba@email.cz>
2025-01-01 23:17:44 +0100

    17a

--- 17/solver.hs
+++ 17/solver.hs
@@ -1,79 +1,49 @@
-import Prelude hiding (lookup)
-import Data.List (minimumBy)
-import Data.Ord (comparing)
+import qualified Data.PQueue.Prio.Min as Q
+import qualified Data.Map as M
+import Data.List (nub, minimumBy)
 
-type Layout = [[Int]]
-type Pos = (Int, Int)
-data Node = Source | Node { pos :: Pos, prev :: Node, cost :: Int }
-  deriving (Show, Eq)
-
-parseLine :: String -> [Int]
-parseLine = map (\c -> read [c])
-
-calculateMinCostPath :: Layout -> IO Int
-calculateMinCostPath layout = dijkstraCost layout targetPos queue where
-  source = Node { pos=(0,0), prev=Source, cost=0 }
-  targetPos = (length layout - 1, length (head layout) - 1)
-  queue = [source]
-
-dijkstraCost :: Layout -> Pos -> [Node] -> IO Int
-dijkstraCost layout target queue
-  | pos minCostNext == target = return $ cost minCostNext
-  | otherwise = do 
-    let queueWithoutMin = removeFromQueue minCostNext queue
-    let neighbors = findNeighbors layout minCostNext
-    let updatedQueue = queueWithoutMin ++ neighbors
-    print minCostNext
-    print updatedQueue
-    print ""
-    dijkstraCost layout target updatedQueue where
-      minCostNext = findMinCost queue
-
-findMinCost :: [Node] -> Node
-findMinCost = minimumBy (comparing cost)
-
-removeFromQueue :: Node -> [Node] -> [Node]
-removeFromQueue toRemove [] = []
-removeFromQueue toRemove (n:ns)
-  | n == toRemove = ns
-  | otherwise = n : removeFromQueue toRemove ns
-
-findNeighbors :: Layout -> Node -> [Node]
-findNeighbors layout node = filter (isValid layout node) neighborCandidates where
-  nodeCost = cost node
-  nodeX = fst $ pos node
-  nodeY = snd $ pos node
-  neighborCandidates =
-    [ Node { pos=(x,nodeY), cost=nodeCost + (layout !! x !! nodeY), prev=node } |
-    x <- [nodeX - 1, nodeX + 1]] ++
-    [ Node { pos=(nodeX, y), cost=nodeCost + (layout !! nodeX !! y), prev=node } |
-    y <- [nodeY - 1, nodeY + 1] ]
-
-isValid :: Layout -> Node -> Node -> Bool
-isValid layout curr next = inBounds && nextNotPrev && notFourthInSameDir where
-  (x, y) = pos next
-  inBounds = x >= 0 && y >= 0 && x < length layout && y < length (head layout)
-  nextNotPrev = prev curr /= next
-  upToLastThree = getLastInPath curr 3
-  notFourthInSameDir = not $ isFourthInSameDir upToLastThree next
-
-getLastInPath :: Node -> Int -> [Node]
-getLastInPath _ 0 = []
-getLastInPath Source _ = []
-getLastInPath node n = node : getLastInPath (prev node) (n-1)
-
-isFourthInSameDir :: [Node] -> Node -> Bool
-isFourthInSameDir ns n = length ns == 3 && (fourXs || fourYs) where
-  nx = fst $ pos n
-  ny = snd $ pos n
-  poss = map pos ns
-  fourXs = and [ nx == fst p | p <- poss ]
-  fourYs = and [ ny == snd p | p <- poss ]
+data Dir = N | W | S | E | Z deriving (Eq, Ord, Show)
+type Coord = (Int, Int)
+type Pos = (Coord, Dir, Int)
+type Queue = Q.MinPQueue Int Pos
 
 main :: IO ()
 main = do
   contents <- getContents
-  let inputLines = lines contents
-  let layout = map parseLine inputLines :: Layout
-  minCostPath <- calculateMinCostPath layout
-  print minCostPath
+  let strs = lines contents
+  let initPos = ((0,0), Z, 0) -- no initial dir faked by north
+  let q = Q.singleton 0 initPos
+  let cs = M.singleton initPos 0
+  let (c, prevs) = dijkstra strs cs M.empty q
+  print c
+
+dijkstra :: [String] -> M.Map Pos Int -> M.Map Pos Pos -> Queue -> (Int, M.Map Pos Pos)
+dijkstra strs costs prevs q
+  | fst3 minCoord == (length strs - 1, length strs - 1) = (minCost, prevs)
+  | otherwise = dijkstra strs costs' prevs' q' where
+    (minCost, minCoord) = Q.findMin q
+    q0 = Q.deleteMin q
+    succs = filter (\(c,p) -> c < M.findWithDefault (10^6) p costs)
+      $ map (\((x,y), d, n) -> (minCost + read [strs !! x !! y], ((x,y), d, n)))
+      $ filter (\((x,y), d, n) -> n < 3)-- -> x /= invalidX && y /= invalidY && (x,y) /= minCoord)
+      $ neighbors strs minCoord
+    costs' = foldl (\acc (c,p) -> M.insert p c acc) costs succs
+    prevs' = foldl (\acc (_,p) -> M.insert p minCoord acc) prevs succs
+    q' = foldl (\acc (c,p) -> Q.insert c p acc) q0 succs
+
+fst3 :: Pos -> (Int, Int) 
+fst3 (c,_,_) = c
+
+neighbors :: [String] -> Pos -> [Pos]
+neighbors strs ((x,y), d, n) = 
+  map (\(coor, d') -> if d' == d then (coor, d', n+1) else (coor, d', 0))
+  $ filter (\((x',y'), d') -> x' >= 0 && x' < s && y' >= 0 && y' < s && d' /= opp d) 
+    [((x-1, y), N), ((x+1,y), S), ((x,y-1), W), ((x,y+1), E)]
+  where s = length strs
+
+opp :: Dir -> Dir
+opp N = S
+opp S = N
+opp E = W
+opp W = E
+opp Z = Z
