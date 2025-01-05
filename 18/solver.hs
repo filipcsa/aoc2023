ca1771c62016f29d735a5a49b24947179249165b
Csaba Filip <filip.csaba@email.cz>
2025-01-05 21:37:21 +0100

    18b

--- 18/solver.hs
+++ 18/solver.hs
@@ -1,5 +1,6 @@
 import Prelude hiding (lookup)
 import Data.List.Split (splitOn)
+import Data.Char (ord)
 
 type Move = (Char, Int)
 type Coord = (Int, Int)
@@ -9,8 +10,11 @@ main = do
   contents <- getContents
   let inputLines = lines contents
   let moves = map parseMove inputLines
+  let moves' = map parseMove' inputLines
   let coords = foldl makeCoord [(0,0)] moves
+  let coords' = foldl makeCoord [(0,0)] moves'
   print $ computeArea coords
+  print $ computeArea coords'
 
 computeArea :: [Coord] -> Int
 computeArea coords = a' + (c `div` 2) + 1 where
@@ -31,5 +35,22 @@ parseMove str = (head dirStr, read numStr) where
   [dirStr, numStr, _] = splitOn " " str
 
 parseMove' :: String -> Move
-parseMove' str = (head dirStr, read numStr) where
-  [dirStr, numStr, _] = splitOn " " str
+parseMove' str = (resolveDir hexStr, parseHexStr hexStr) where
+  [_, _, hexStr] = splitOn " " str
+
+resolveDir :: [Char] -> Char
+resolveDir str = case c of
+  '0' -> 'R'
+  '1' -> 'D'
+  '2' -> 'L'
+  '3' -> 'U'
+  where c = str !! (length str - 2)
+
+parseHexStr :: [Char] -> Int
+parseHexStr str = foldl (\acc c -> 16*acc + hexCharToInt c) 0 (drop 2 $ init $ init str)
+
+hexCharToInt :: Char -> Int
+hexCharToInt c 
+  | ord c >= ord '0' && ord c <= ord '9' = (ord c) - (ord '0')
+  | otherwise = 10 + ord c - ord 'a'
+
