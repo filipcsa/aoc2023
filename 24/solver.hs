6e33e95bb5f69bac35155d8066ea567c3d3b3771
Csaba Filip <filip.csaba@email.cz>
2025-01-19 20:54:35 +0100

    24a basis

--- 24/solver.hs
+++ 24/solver.hs
@@ -2,19 +2,33 @@ import Data.List.Split (splitOn)
 type Pos = (Int, Int, Int)
 type Vel = (Int, Int, Int)
 
+minArea :: Int
+minArea = 7
+
+maxArea :: Int
+maxArea = 0
+
 main :: IO ()
 main = do
   inputLines <- fmap lines getContents
   let possAndVels = map parseInput inputLines
-  let crossPos2D = [ findCrossPos pv1 pv2 | pv1 <- possAndVels, pv2 <- possAndVels, pv1 /= pv2 ]
-  print $ length $ filter validRange crossPos2D
+  let crossTimes = [ findCrossTime pv1 pv2 | pv1 <- possAndVels, pv2 <- possAndVels, pv1 /= pv2 ]
+  print crossTimes
+  -- print $ length $ filter validRange crossTimes
 
 validRange :: Maybe (Int, Int) -> Bool
 validRange Nothing = False
 validRange _ = False
 
-findCrossPos :: (Pos, Vel) -> (Pos, Vel) -> Maybe (Int, Int)
-findCrossPos _ _ = Nothing
+-- x1 + tvx1 = x2 + tvx2
+-- t = (x1-x2) / (vx2 - vx1)
+findCrossTime :: (Pos, Vel) -> (Pos, Vel) -> Maybe Double
+findCrossTime ((x1,y1,_),(vx1,vy1,_)) ((x2,y2,_),(vx2,vy2,_)) = case (computeT x1 vx1 x2 vx2,computeT y1 vy1 y2 vy2) of
+  (Just t1, Just t2) -> if abs (t1 - t2) < 10 then Just t1 else Nothing
+  _ -> Nothing 
+
+computeT :: Int -> Int -> Int -> Int -> Maybe Double
+computeT x1 vx1 x2 vx2 = if vx1 == vx2 then Nothing else Just $ fromIntegral (x1-x2) / fromIntegral (vx2-vx1)
 
 parseInput :: String -> (Pos, Vel)
 parseInput str = ((x,y,z), (vx,vy,vz)) where
